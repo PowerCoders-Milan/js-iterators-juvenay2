46d0b323dd4d718a195200237a88e289011cfb77
rinaldi <juvenay2@gmail.com>
2021-06-21 18:02:34 +0200

    done

--- 01-for-each/for_each.js
+++ 01-for-each/for_each.js
@@ -3,3 +3,10 @@
 // .forEach((fruit, index) => {
     // console.log() a message interpolating the name of the fruit and it's position in the array (index increased by 1);
 // })
+
+const fruits = ['apple','banana','mango'];
+
+fruits.forEach((fruit, index) => {
+    console.log('my fruit is: ', index, fruit);
+})
+
