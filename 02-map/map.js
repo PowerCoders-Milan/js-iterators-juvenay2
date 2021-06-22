7a221c5db8c8f25dfb372545e81cc84f44c98478
rinaldi <juvenay2@gmail.com>
2021-06-23 00:00:48 +0200

    first part done

--- 02-map/map.js
+++ 02-map/map.js
@@ -1,13 +1,33 @@
-const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+ const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 
-const secretMessage = // use .map to get the first letter of every animal name in the arrray animals
-
+ // use .map to get the first letter of every animal name in the arrray animals
+ const secretMessage = animals.map(animal => animal[0].split(''));
 console.log(secretMessage.join(''));
 
 
-const bigNumbers = [100, 200, 300, 400, 500];
+// const upperCaseWords = words.map(word => word.toUpperCase());
+
+// console.log(upperCaseWords);
+
+// const capitalizeWord = word => {
+//   let wordSplitted = word.toLowerCase().split('');
+//   wordSplitted[0] = wordSplitted[0].toUpperCase();
+//   console.log(wordSplitted.join(''))
+//   return wordSplitted.join('')
+// }
+
+
+
+
+
+
+
+
+
+
+// const bigNumbers = [100, 200, 300, 400, 500];
 
 // Create the smallNumbers array below with map
 
 
-console.log(smallNumbers);
+// console.log(smallNumbers);
